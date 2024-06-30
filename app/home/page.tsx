bb6ae27202aa85e165e451f2396003473c0eb868
M Adithya Vardhan Reddy <mavr10.7.004@gmail.com>
2024-07-01 03:57:40 +0530

    test

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const page = async (props: Props) => {
   return (
     <div>
-      <div className="w-screen"></div>
+      <div className="w-screen">home</div>
     </div>
   );
 };
